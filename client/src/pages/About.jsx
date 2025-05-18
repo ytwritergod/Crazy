cdde25ec602ecc5bb3f3bd6a51b8643900bc5f98
ytwritergod <ytwritergod@gmail.com>
2025-05-18 16:12:59 +0530

    Create About.jsx

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">About THE ANSH</h1>
+      <p className="text-lg">
+        THE ANSH is a platform built for friends to experience excellence. Download videos from multiple platforms and access free courses in various categories!
+      </p>
+    </div>
+  );
+}
+
+export default About;
