c8a707dbd420aaef04fb10bd1e2aeec674ff25cf
ytwritergod <ytwritergod@gmail.com>
2025-05-18 16:13:26 +0530

    Create Owner.jsx

---
+++ client/src/pages/Owner.jsx
@@ -0,0 +1,12 @@
+function Owner() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Owner</h1>
+      <p className="text-lg">
+        THE ANSH was created by a passionate developer for friends to enjoy seamless video downloading and free courses. Stay tuned for updates!
+      </p>
+    </div>
+  );
+}
+
+export default Owner;
